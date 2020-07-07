26d247c7d07f880a33a6a077d1890cb929e3e632
ChouJinshi <zhaochenzhiz@outlook.com>
2020-07-07 21:45:16 +0800

    创建项目

---
+++ src/main/java/org/sdjusei/sdjulife/SdjulifeApplication.java
@@ -0,0 +1,13 @@
+package org.sdjusei.sdjulife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjulifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjulifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sdjusei/sdjulife/SdjulifeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sdjusei.sdjulife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjulifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
