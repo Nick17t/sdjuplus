3dfcdf257093be62e19038cf14c85e70d527fd43
ChouJinshi <zhaochenzhiz@outlook.com>
2020-08-31 23:08:47 +0800

    补上漏写的注解

--- src/main/java/org/sdjusei/sdjulife/service/BusInfoService.java
+++ src/main/java/org/sdjusei/sdjulife/service/BusInfoService.java
@@ -2,10 +2,12 @@ package org.sdjusei.sdjulife.service;
 
 import org.sdjusei.sdjulife.dao.BusInfoDao;
 import org.sdjusei.sdjulife.model.domain.BusInfo;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@Service
 public class BusInfoService {
 
 	@Resource
